a62aab4fb1bd1f13f328fabf64bb095a8e912597
Olivier MARTY <marty.olivier@gmail.com>
2016-11-01 02:00:29 +0100

    flat owners dynamic pages ok

--- config.rb
+++ config.rb
@@ -30,6 +30,9 @@ activate :autoprefixer
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
+data.flats.each do |owner, flat|
+  proxy "/flats/#{owner}.html", "/flats/show.html", :locals => { :owner => owner }, :ignore => true
+end
 
 ###
 # Helpers
